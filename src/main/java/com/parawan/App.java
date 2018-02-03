764ac0cce7853450ad626b2fd3bf272f237ce1cd
Szymon Truszczyński <struszczynski@o2.pl>
2018-02-04 00:20:51 +0100

    Added few features in text menu in App.java. From now on during the reservation process, console will inform us if place isnt already reserved or dirty. Added Try-Catch expression to avoid compilation error

--- src/main/java/com/parawan/App.java
+++ src/main/java/com/parawan/App.java
@@ -5,8 +5,10 @@ import com.sun.xml.internal.bind.v2.model.core.ID;
 import javax.xml.bind.JAXBException;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import static java.lang.Thread.sleep;
 
 
 public class App {
@@ -26,26 +28,48 @@ public class App {
 
         while (flag) {
 
-            System.out.println("Private beach management system - PARAWAN.              ver1.0");
-            System.out.println("\nPlease specify Your action.\n");
+            System.out.println("\nPrivate beach management system - PARAWAN.              ver1.0");
+            System.out.println("Please specify Your action.\n");
             System.out.println("Would You like to: [r]eserve place, [c]ancel reservation, [s]ee current beach preview, \nc[h]eck storehouse status, [q]uit program");
 
             String answer = scanner.nextLine();
 
             if(answer.equals("r")){
-                System.out.println("Please specify ID number from 1 to 200 to make reservation");
-                int idNumber =scanner.nextInt();
-                sunnyBeach.getPlaces().get(idNumber-1).setStatus(PlaceStatus.RESERVED);
+                System.out.println("Please specify ID number from "+sunnyBeach.places.get(0).getId() +" to "+sunnyBeach.places.get(sunnyBeach.places.size()-1).getId()+" to make reservation");
+                try {
+                    int idNumber = scanner.nextInt();
+
+                    if(sunnyBeach.places.get(idNumber-1).getStatus()==PlaceStatus.DIRTY){
+                        System.out.println("Sorry, but right now this place is dirty and cannot be reserved ");
+                    }
+                    else if(sunnyBeach.places.get(idNumber-1).getStatus()==PlaceStatus.RESERVED){
+                        System.out.println("Sorry, but this place is already reserved at that time");
+
+                    }
+                    else {
+                        sunnyBeach.getPlaces().get(idNumber - 1).setStatus(PlaceStatus.RESERVED);
+                    }
+                }
+                catch (Exception e){
+                    System.out.println("Please be sure to type Integer within the bounds");
+                }
+
             }
             else if(answer.equals("c")){
-                System.out.println("Please specify ID number from 1 to 200 to cancel reservation");
-                int idNumberCancel=scanner.nextInt();
-                sunnyBeach.getPlaces().get(idNumberCancel-1).setStatus((PlaceStatus.FREE));
+                System.out.println("Please specify ID number from "+sunnyBeach.places.get(0).getId()+" to "+sunnyBeach.places.get(sunnyBeach.places.size()-1).getId()+" to cancel reservation");
+
+                try {
+                    int idNumberCancel = scanner.nextInt();
+                    sunnyBeach.getPlaces().get(idNumberCancel - 1).setStatus((PlaceStatus.FREE));
+                }
+                catch (Exception e){
+                    System.out.println("Please be sure to type Integer within the bounds");
+                }
 
             }
             else if(answer.equals("s")){
                 for(int i=0; i<sunnyBeach.places.size(); i++)
-                System.out.println(sunnyBeach.places.get(i));
+                    System.out.println(sunnyBeach.places.get(i));
             }
 
             else if(answer.equals("q")){
