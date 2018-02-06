21ca153d4728889a66575f16a1854fd65a8c9320
Bartłomiej Stępień <bartlomiej.korespondencja@gmail.com>
2018-02-06 11:20:47 +0100

    updated the XML Writer

--- src/main/java/com/parawan/JavaToXML.java
+++ src/main/java/com/parawan/JavaToXML.java
@@ -4,6 +4,9 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.PropertyException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +25,19 @@ public class JavaToXML {
             }
         }
 
-        PlacesWrapper placesWrapper = new PlacesWrapper();
-        placesWrapper.setPlaces(places);
-        m.marshal(placesWrapper, System.out);
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream("./moj_output.xml");
+            PlacesWrapper placesWrapper = new PlacesWrapper();
+            placesWrapper.setPlaces(places);
+            m.marshal(placesWrapper, fileOutputStream);
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
