936f70d599f820cdf4df1aaa7d406fa6002578cc
Szymon Truszczyński <34796413+trueH3@users.noreply.github.com>
2018-02-12 15:27:04 +0100

    Jjddit 3.ver2 (#11)
    * I made some code refactor. Removed code that was responsible for displaying menu from App.java class and created 5 classes in new package com.menu. I also connected classes: SearchEngine and ReservationPreview to main menu
    
    * Fixed minor bug in new menu. From now on writing data to xml works properly
    
    * Changed Date to LocalDateTime in MainMenu Class. Made code formatting

---
+++ src/main/java/com/parawan/com/menu/CancelReservation.java
@@ -0,0 +1,19 @@
+package com.parawan.com.menu;
+
+import com.parawan.Beach;
+import com.parawan.PlaceStatus;
+
+import java.util.Scanner;
+
+public class CancelReservation {
+    public void undoReservation(Beach beach, Scanner scanner) {
+        System.out.println("Please specify ID number from " + beach.getPlaces().get(0).getId() + " to " + beach.getPlaces().get(beach.getPlaces().size() - 1).getId() + " to cancel reservation");
+
+        try {
+            int idNumberCancel = Integer.parseInt(scanner.nextLine());
+            beach.getPlaces().get(idNumberCancel - 1).setStatus((PlaceStatus.FREE));
+        } catch (Exception e) {
+            System.out.println("Please be sure to type Integer within the bounds");
+        }
+    }
+}
--- src/main/java/com/parawan/com/menu/ReservationWithRequirements.java
+++ src/main/java/com/parawan/com/menu/ReservationWithRequirements.java
@@ -1,35 +1,35 @@
-package com.parawan;
+package com.parawan.com.menu;
+
+import com.parawan.ItemType;
+import com.parawan.Place;
+import com.parawan.PlaceStatus;
 
 import java.util.Scanner;
 
-public class SearchEnginePlaceBuilder {
+public class ReservationWithRequirements {
 
     private String option;
     private Scanner sc = new Scanner(System.in);
     private Place placeToAvoid = new Place();
 
     public Place getRequirements() {
-        System.out.println("Do you want no neighbours?\nType Y:");
-        option = sc.nextLine().toLowerCase();
+        System.out.println("Do you want place with no neighbours around? Please type [y]es or [n]o :");
+        option = sc.nextLine();
         if (option.equals("y")) {
             placeToAvoid.setStatus(PlaceStatus.RESERVED);
         } else {
             placeToAvoid.setStatus(PlaceStatus.FREE);
         }
-        System.out.println("Do you want no PARAWANS nearby?\n" +
-                "Type Y to confirm");
-        option = sc.nextLine().toLowerCase();
+        System.out.println("Do you want place with no PARAWANS around? Please type [y]es or [n]o :");
+        option = sc.nextLine();
         if (option.equals("y")) {
             placeToAvoid.putItems(ItemType.SCREEN, 1);
         }
-        System.out.println("Do you want no umbrellas nearby?\n" +
-                "Type Y/N");
+        System.out.println("Do you want place with no umbrellas around? Please type [y]es or [n] :");
         option = sc.nextLine().toLowerCase();
         if (option.equals("y")) {
             placeToAvoid.putItems(ItemType.UMBRELLA, 1);
         }
         return placeToAvoid;
     }
-
-
 }
