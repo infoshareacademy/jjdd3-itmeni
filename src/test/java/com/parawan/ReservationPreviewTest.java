8785c777cf2fde764da7e5368d31781bfe430e54
Daniel Żychniewicz <jagda.1900@wp.pl>
2018-02-13 13:06:47 +0100

    removed - booking_status.xml-develop; added unit test for class reservation prieview

--- src/test/java/com/parawan/ReservationPreviewTest.java
+++ src/test/java/com/parawan/ReservationPreviewTest.java
@@ -6,14 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationPreviewTest {
 
-
     @Test
-    void shouldThrowExceptionWhenPrintedParemeters() {
-        Throwable exception =
-                assertThrows(IllegalArgumentException.class, () -> {
-                    ;
-                });
-        assertEquals(, exception.getMessage(),"Wow-out of range!");
+    void shouldThrowExceptionWhenParametersAreOutOfRange() {
+        Beach beach = new Beach(10, 10);
+        ReservationPreview reservationPreview = new ReservationPreview();
 
+        assertThrows(IllegalArgumentException.class, () -> {
+            reservationPreview.getPlaceByCoordinates(beach, 19, 4);
+        });
     }
 }
