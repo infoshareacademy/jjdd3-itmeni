450aaed2e46572c6e24332993f51c14323b39d48
Daniel Żychniewicz <jagda.1900@wp.pl>
2018-02-13 17:00:09 +0100

    add link to datebase

--- src/main/java/com/parawan/App.java
+++ src/main/java/com/parawan/App.java
@@ -5,14 +5,26 @@ import com.parawan.com.menu.*;
 
 public class App {
     public static void main(String[] args) {
-        
+        String path = "src/test/resources/booking_status.xml";
+        if (args.length != 1) {
+            System.out.println("Please provide input file.");
+            System.exit(1);
+        }
+        else {
+            path = args[0];
+            System.out.println("INPUT FILE: " + path);
+        }
+        String firstCommandLineArgument = args[0];
+
+
         Beach sunnyBeach = new Beach(20, 10);
         sunnyBeach.createPlaces();
 
         JavaToXML myXMLWriter = new JavaToXML();
 
         XMLToJava myXMLReader = new XMLToJava();
-        Places places = myXMLReader.xmlToJava();
+
+        Places places = myXMLReader.xmlToJava(path);
         sunnyBeach.setPlaces(places);
 
         MainMenu mainMenu = new MainMenu();
--- src/test/java/com/parawan/ReservationPreviewTest.java
+++ src/test/java/com/parawan/ReservationPreviewTest.java
@@ -1,16 +1,34 @@
 package com.parawan;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationPreviewTest {
 
+    private XMLToJava subject;
+    private ReservationPreview reservationPreview;
+
+    @BeforeEach
+    void setUp(){
+        subject = new XMLToJava();
+    }
+
+    @Test
+    void shouldIndicateCompleteInformationAboutBookedPoint() {
+        Places places = subject.xmlToJava("src/test/resources/booking_status.xml");
+        Beach beach = new Beach(2, 1);
+        beach.setPlaces(places);
+        reservationPreview = new ReservationPreview();
+        assertTrue(reservationPreview.getPlaceByCoordinates(beach, 0, 0).getStatus() == PlaceStatus.FREE);
+    }
+
     @Test
     void shouldThrowExceptionWhenParametersAreOutOfRange() {
-        Beach beach = new Beach(10, 10);
-        ReservationPreview reservationPreview = new ReservationPreview();
-
+        Beach beach = new Beach(12, 10);
+        reservationPreview = new ReservationPreview();
         assertThrows(IllegalArgumentException.class, () -> {
             reservationPreview.getPlaceByCoordinates(beach, 19, 4);
         });
