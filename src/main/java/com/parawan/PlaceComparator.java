47f6419ef6d74cca70646c501865ab58f2dd599e
Krzysztof Syska <prezydium834@gmail.com>
2018-02-16 16:13:40 +0100

    PlaceComparator fix - now works

--- src/main/java/com/parawan/PlaceComparator.java
+++ src/main/java/com/parawan/PlaceComparator.java
@@ -15,21 +15,22 @@ public class PlaceComparator {
 
     public boolean comparePlaces(Place placeMain, Place placeToCompare) {
 
-        if (placeMain.getStatus() == placeToCompare.getStatus()) {
+        if (placeMain.getStatus() == placeToCompare.getStatus()
+                && !(placeMain.getStatus() == PlaceStatus.FREE && placeToCompare.getStatus() == PlaceStatus.FREE)) {
             return true;
         }
 
         for (int i = 0; i < ItemType.values().length; i++) {
-             if ((placeMain.getRentedItems().contains(ItemType.values()[i])
-                    && placeToCompare.getRentedItems().contains(ItemType.values()[i]))){
-                 return true;
+            if (placeMain.getRentedItems().contains(ItemType.values()[i])
+                    && placeToCompare.getRentedItems().contains(ItemType.values()[i])) {
+                return true;
             }
         }
         return false;
     }
 
     public boolean placeNotExist(int x, int y) {
-        return  (x < 0 || x >= beach.getMaxWidth() || y < 0 || y >= beach.getMaxHeight());
+        return (x < 0 || x >= beach.getMaxWidth() || y < 0 || y >= beach.getMaxHeight());
     }
 
 }
