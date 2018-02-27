927954a2186f63e0a46339e6bb0acd618a1a746f
Szymon Truszczyński <struszczynski@o2.pl>
2018-02-27 09:36:29 +0100

    Added 2 unity tests for MainMenu (#27)

--- src/main/java/com/parawan/com/menu/ItemManagement.java
+++ src/main/java/com/parawan/com/menu/ItemManagement.java
@@ -3,20 +3,21 @@ package com.parawan.com.menu;
 import com.parawan.ItemType;
 import com.parawan.reservation.ReservationTable;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class ItemManagement {
-    public void itemCount(ReservationTable reservationTable, Scanner scanner) {
 
-        int typedHour = 0;
-        int rentedScreenCount = 0;
-        int rentedUmbrellaCount = 0;
-        int rentedTowelCount = 0;
-        int rentedSunbedCount = 0;
-        int amountOfScreen = 200;
-        int amountOfUmbrella = 200;
-        int amountOfTowel = 200;
-        int amountOfSunbed = 200;
+    private int typedHour = 0;
+    private int amountOfScreen = 200;
+    private int amountOfUmbrella = 200;
+    private int amountOfTowel = 200;
+    private int amountOfSunbed = 200;
+
+    public void setTypedHour(int typedHour) {
+        this.typedHour = typedHour;
+    }
+
+    public void itemCount(ReservationTable reservationTable, Scanner scanner) {
 
         while (typedHour < 8 || typedHour > 19) {
             System.out.println("Please type hour that interest You  (Beach is open from 8.00 to 19.00)");
@@ -27,6 +28,16 @@ public class ItemManagement {
                 System.out.println("Please be sure to type Integer within the bounds");
             }
         }
+
+        forRentAtGivenHour(reservationTable);
+    }
+
+    public int [] forRentAtGivenHour(ReservationTable reservationTable) {
+
+        int rentedScreenCount = 0;
+        int rentedUmbrellaCount = 0;
+        int rentedTowelCount = 0;
+        int rentedSunbedCount = 0;
 
         for (int i = 0; i < reservationTable.size(); ++i) {
             if (reservationTable.get(i).getHourOfReservation() == typedHour) {
@@ -50,10 +61,14 @@ public class ItemManagement {
         int towelForRentAtGivenHour = amountOfTowel - rentedTowelCount;
         int sunbedForRentAtGivenHour = amountOfSunbed - rentedSunbedCount;
 
-        System.out.println("At " + typedHour + " o'clock we have:\n ");
+        int [] stillForRent = {screenForRentAtGivenHour, umbrellaForRentAtGivenHour, towelForRentAtGivenHour, sunbedForRentAtGivenHour};
+
+        System.out.println("At " + typedHour + ":00 we have:\n ");
         System.out.println(screenForRentAtGivenHour + " screens available for rent");
         System.out.println(umbrellaForRentAtGivenHour + " umbrellas available for rent");
         System.out.println(towelForRentAtGivenHour + " towels available for rent");
         System.out.println(sunbedForRentAtGivenHour + " sunbeds available for rent");
+
+        return stillForRent;
     }
 }
