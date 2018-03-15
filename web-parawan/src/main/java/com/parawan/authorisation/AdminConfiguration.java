7d03c69d3f96179da9723ca2e46cf58a2ed399ae
BartekOliwaPL <bartlomiej.korespondencja@gmail.com>
2018-03-15 02:26:51 +0100

    Started creating the Login page and servlet with Google API

---
+++ web-parawan/src/main/java/com/parawan/authorisation/AdminConfiguration.java
@@ -0,0 +1,18 @@
+package com.parawan.authorisation;
+
+import java.util.List;
+
+public class AdminConfiguration {
+    private List<String> admins;
+
+    public AdminConfiguration() {
+    }
+
+    public AdminConfiguration(List<String> admins) {
+        this.admins = admins;
+    }
+
+    public List<String> getAdmins() {
+        return admins;
+    }
+}
