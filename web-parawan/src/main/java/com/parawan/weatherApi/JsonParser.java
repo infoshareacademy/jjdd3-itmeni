d3f2ea8d6b52eb30c31b1327baf70b8dbcb535b9
Krzysztof Syska <prezydium834@gmail.com>
2018-03-23 12:21:15 +0100

    Forecast is now Stateful EJB

--- web-parawan/src/main/java/com/parawan/weatherApi/JsonParser.java
+++ web-parawan/src/main/java/com/parawan/weatherApi/JsonParser.java
@@ -1,17 +1,19 @@
 package com.parawan.weatherApi;
 
+import javax.ejb.Stateful;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Stateful
 public class JsonParser {
 
     public WeatherForecastResponse getWeatherForecast() {
 
         Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("https://178.62.207.82/data/2.5/forecast?APPID=911ddd6b9e551144f9e93318230dd105&id=7531890&lang=en&units=metric&mode=json");
+        WebTarget target = client.target("https://api.openweathermap.org/data/2.5/forecast?APPID=911ddd6b9e551144f9e93318230dd105&id=7531890&lang=en&units=metric&mode=json");
         Response response = target.request().accept(MediaType.APPLICATION_JSON_TYPE).get();
         WeatherForecastResponse value = response.readEntity(WeatherForecastResponse.class);
         response.close();
